5275ab76e15d9281d11d05fa2e01040d527cc307
santyago1802 <sm.marinez.ms18@gmail.com>
2026-06-15 18:26:20 -0500

    Agrega ejercicios py con POO y actualiza interfaz

--- Stockia/src/pages/Productos.tsx
+++ Stockia/src/pages/Productos.tsx
@@ -18,7 +18,25 @@ function Productos() {
             </div>
 
             <div className="products-section">
-                <div className="products-grid">
+                <div className="card-grid">
+                    <div className="card">
+                        <img src="https://compucentro.co/wp-content/uploads/D_NQ_NP_2X_849908-MLA91863387531_092025-F.png" alt="imagen de material" className="card-img"/>
+                        <div className="card-body">
+                            <h6 className="card-title">Card Title</h6>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <button className="btn btn-primary">Solicitar</button>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://compucentro.co/wp-content/uploads/D_NQ_NP_2X_849908-MLA91863387531_092025-F.png" alt="imagen de material" className="card-img"/>
+                        <div className="card-body">
+                            <h6 className="card-title">Card Title</h6>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <button className="btn btn-primary">Solicitar</button>
+                        </div>
+                    </div>
+
                     <div className="card">
                         <img src="https://compucentro.co/wp-content/uploads/D_NQ_NP_2X_849908-MLA91863387531_092025-F.png" alt="imagen de material" className="card-img"/>
                         <div className="card-body">
