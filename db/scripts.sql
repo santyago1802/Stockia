a85d18f6b94f9c6e8064052518a3c2b04d0cd9c5
Santyago <sm.marinez.ms18@gmail.com>
2026-08-27 14:19:05 -0500

    se hace la actuazlizcion de la base de datos

--- db/scripts.sql
+++ db/scripts.sql
@@ -39,35 +39,46 @@ CREATE TABLE material (
     id_material INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nombre_material VARCHAR(100) UNIQUE NOT NULL,
     descripcion_material TEXT,
-    imagen_material VARCHAR(255),
-    cantidad_material INTEGER CHECK (cantidad_material >= 0)  NOT NULL,
-    stock_minimo INTEGER CHECK (stock_minimo >= 0) NOT NULL,
-    fecha_ingreso TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    id_proveedor INTEGER REFERENCES proveedor(id_proveedor) NOT NULL,
     id_categoria INTEGER REFERENCES categoria(id_categoria) NOT NULL
 );
-
-CREATE TABLE prestamo_material (
-    id_prestamo INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+CREATE TABLE detalle_proveedor (
+    id_detalle_proveedor INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     id_material INTEGER REFERENCES material(id_material) NOT NULL,
+    id_proveedor INTEGER REFERENCES proveedor(id_proveedor) NOT NULL,
+    fecha_compra TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    stock_minimo INTEGER CHECK (stock_minimo >= 0) NOT NULL,
+    cantidad INTEGER CHECK (cantidad >= 0) NOT NULL
+);
+
+
+CREATE TABLE prestamo (
+    id_prestamo INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     id_usuario INTEGER REFERENCES usuario(id_usuario) NOT NULL,
     fecha_prestamo TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     fecha_devolucion TIMESTAMP,
-    cantidad_prestada INTEGER CHECK (cantidad_prestada > 0) NOT NULL,
-    cantidad_devuelta INTEGER NOT NULL DEFAULT 0,
-    CONSTRAINT chk_cantidad_devuelta
-    CHECK (cantidad_devuelta >= 0 AND cantidad_devuelta <= cantidad_prestada)
+    estado VARCHAR(10) CHECK (estado IN ('pendiente', 'devuelto')) NOT NULL
 );
 
-CREATE TABLE movimiento_inventario (
-    id_movimiento INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+CREATE TABLE detalle_prestamo (
+    id_prestamo INTEGER REFERENCES prestamo(id_prestamo) NOT NULL,
     id_material INTEGER REFERENCES material(id_material) NOT NULL,
-    tipo_movimiento VARCHAR(10) 
-        CHECK (tipo_movimiento IN ('entrada', 'salida')) NOT NULL,
-    cantidad_movimiento INTEGER
-        CHECK (cantidad_movimiento > 0) NOT NULL,
+    cantidad INTEGER CHECK (cantidad > 0) NOT NULL
+);
+
+CREATE TABLE movimiento (
+    id_movimiento INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id_prestamo INTEGER REFERENCES prestamo(id_prestamo),
+    id_material INTEGER REFERENCES material(id_material) NOT NULL,
+    cantidad INTEGER
+        CHECK (cantidad > 0) NOT NULL,
+    observacion TEXT,
     fecha_movimiento TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     id_usuario INTEGER REFERENCES usuario(id_usuario) NOT NULL
 );
 
+CREATE TABLE tipo_movimiento (
+    id_tipo_movimiento INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    tipo_movimiento VARCHAR(10) 
+        CHECK (tipo_movimiento IN ('entrada', 'salida')) NOT NULL,
+);
 
